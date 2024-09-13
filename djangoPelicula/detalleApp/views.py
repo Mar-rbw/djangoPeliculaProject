66f3dfdf244882be197dc88bae57f0968b70f090
Robowa <marceli3@outlook.es>
2024-09-13 12:06:00 -0300

    Otra modificaciion

--- djangoPelicula/detalleApp/views.py
+++ djangoPelicula/detalleApp/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def detallesGot(request):
+    data = {
+        "titulo": "Game of thorns",
+        "descripcion": "Mala temporada 8",
+        "valor" : "8"
+    }
+    return(request, "detalleApp.html", data)
+
+def detallesPb(request):
+    data = {
+        "titulo": "prison Break",
+        "descripcion": "No se",
+        "valor" : "5"
+    }
+    return(request, "detalleApp.html", data)
+
+def detallesBa(request):
+    data = {
+        "titulo": "Breking Bad",
+        "descripcion": "JESSSI!!",
+        "valor" : "1000/10"
+    }
+    return(request, "detalleApp.html", data)
